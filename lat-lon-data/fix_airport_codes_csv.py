132b1a3ebd2bcdec6955c9bbf593f800e1c50d56
Nithin Chilakapati <nithinchilakapati@gmail.com>
2023-12-27 00:30:37 -0800

    Corrected math calculation and updated README

--- lat-lon-data/fix_airport_codes_csv.py
+++ lat-lon-data/fix_airport_codes_csv.py
@@ -2,10 +2,10 @@
 
 import pandas as pd
 
-df = pd.read_csv("airport-codes.csv")
+df = pd.read_csv("lat-lon-data/airport-codes.csv")
 df = df[df.iso_country == "US"]
 
 # df.column_name != whole string from the cell
 # now, all the rows with the column: Name and Value: "dog" will be deleted
 
-df.to_csv("us-airport-codes.csv", index=False)
+df.to_csv("lat-lon-data/us-airport-codes.csv", index=False)
